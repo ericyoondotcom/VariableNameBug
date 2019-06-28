3e075461dc4632a7be79ee0084088ab66c586776
Eric <emjyoon@gmail.com>
2019-06-28 11:15:51 -0700

    Init

--- VariableNameBug/Program.cs
+++ VariableNameBug/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
 
 namespace VariableNameBug
 {
@@ -6,7 +8,18 @@ namespace VariableNameBug
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] Stack = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(Stack.GetType());
+            //System.Int32[]
+            //This proves that the variable Stack is definitely an int array
+
+            System.Diagnostics.Debugger.Break();
+            //However, on debug, instead of showing useful info, hovering over Stack shows:
+            //error CS0234: The type or namespace name 'Stack' does not exist in the namespace 'System.Collections.Generic' (are you missing an assembly reference?)
+
+            //The debugger seems to prefer the class Stack, imported from System.Collections.Generic
+            //However, C# correctly uses the local variable
         }
     }
 }
